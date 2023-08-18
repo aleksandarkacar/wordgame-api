105dc2991c015318027c687c0292680948d4c977
AleksandarKacar <kacar95@gmail.com>
2023-08-18 11:37:31 +0200

    api should be done

---
+++ app/Http/Controllers/WordController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\WordRequest;
+use Illuminate\Support\Facades\Http;
+
+use function App\Helpers\palindromeChecker;
+
+class WordController extends Controller
+{
+    public function verifyWord(WordRequest $request)
+    {
+        $validatedData = $request->validated();
+        $word = $validatedData["word"];
+        $cleanedStr = strtolower(preg_replace('/[^A-Za-z0-9]/', '', $word));
+
+        $apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/$cleanedStr";
+
+        $response = Http::get($apiUrl);
+
+        if (!$response->successful()) {
+                return response()->json(['valid' => false]);
+        }
+
+        $checkPalindrome = palindromeChecker($cleanedStr);
+
+        $uniqueLettersCount = count(array_unique(str_split($cleanedStr)));
+
+        $score = $uniqueLettersCount;
+        if ($checkPalindrome == "is a palindrome") {
+            $score += 3;
+        }
+        if ($checkPalindrome == "is almost a palindrome") {
+            $score += 2;
+        }
+
+        return response()->json(['valid' => true, 'palindrome' => $checkPalindrome, 'uniqueLetters' => $uniqueLettersCount, 'score' => $score]);
+    }
+}
+
---
+++ app/helpers/PalindromeHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+Namespace App\Helpers;
+
+function palindromeChecker ($word) {
+    $wordLength = strlen($word);
+
+    for ($i = 0; $i < strlen($wordLength)/2; $i++) {
+        if ($word[$i] !== $word[$wordLength - 1 - $i]){
+            $newWord = substr_replace($word, '', $i, 1);
+            $newWord2 = substr_replace($word, '', $wordLength - 1 - $i, 1);
+            $almost = $newWord === strrev($newWord) || $newWord2 === strrev($newWord2);
+            if ($almost) {
+                return "is almost a palindrome";
+            }
+            return "is not a palindrome";
+        }
+
+    }
+    return "is a palindrome";
+}
