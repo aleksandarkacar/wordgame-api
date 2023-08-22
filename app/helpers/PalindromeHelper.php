19f1e2285717c9a720ed44e4fde072b782c82ed9
Ammo95 <kacar95@gmail.com>
2023-08-22 22:23:08 +0200

    fixed bug in palindromeChecker helper

--- app/helpers/PalindromeHelper.php
+++ app/helpers/PalindromeHelper.php
@@ -1,12 +1,13 @@
 <?php
 
-Namespace App\Helpers;
+namespace App\Helpers;
 
-function palindromeChecker ($word) {
+function palindromeChecker($word)
+{
     $wordLength = strlen($word);
 
-    for ($i = 0; $i < strlen($wordLength)/2; $i++) {
-        if ($word[$i] !== $word[$wordLength - 1 - $i]){
+    for ($i = 0; $i < $wordLength / 2; $i++) {
+        if ($word[$i] !== $word[$wordLength - 1 - $i]) {
             $newWord = substr_replace($word, '', $i, 1);
             $newWord2 = substr_replace($word, '', $wordLength - 1 - $i, 1);
             $almost = $newWord === strrev($newWord) || $newWord2 === strrev($newWord2);
@@ -15,7 +16,6 @@ function palindromeChecker ($word) {
             }
             return "is not a palindrome";
         }
-
     }
     return "is a palindrome";
 }
